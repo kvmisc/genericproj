cd58c4013ff3099178c97d0f8ee0a321224fb0b5
Kevin Wu <kvmisc@163.com>
2019-04-16 20:16:25 +0800

    faslkd

---
+++ GenericProj/Config/XYZAppearance.h
@@ -0,0 +1,9 @@
+//
+//  XYZAppearance.h
+//  GenericProj
+//
+//  Created by Kevin Wu on 21/11/2017.
+//  Copyright © 2017 firefly.com. All rights reserved.
+//
+
+#define kWBFontSizeLarge 18.0
---
+++ GenericProj/Config/XYZNotification.h
@@ -0,0 +1,13 @@
+//
+//  XYZNotification.h
+//  GenericProj
+//
+//  Created by Haiping Wu on 2019/4/16.
+//  Copyright © 2019 firefly.com. All rights reserved.
+//
+
+#ifndef XYZNotification_h
+#define XYZNotification_h
+
+
+#endif /* XYZNotification_h */
